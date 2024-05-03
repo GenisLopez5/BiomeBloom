9453bd66ce16c42a6b87854fb8e98e40805d1092
Gerard <gerardfelez2@gmail.com>
2024-05-03 23:24:12 +0200

    added Neighbours

--- rust_side/src/lib.rs
+++ rust_side/src/lib.rs
@@ -5,9 +5,14 @@ pub struct DAtom {
     material: u64,
     obsolete: bool,
 }
+
+
+
+type EntityTag = u64;
 #[repr(C)]
+#[derive(Clone, Copy, Debug)]
 pub struct Atom {
-    entity_tag: u64,
+    entity_tag: EntityTag,
 }
 
 static LOGIC_BUFFER: Mutex<Vec<Atom>> = Mutex::new(Vec::new());
@@ -19,4 +24,40 @@ pub extern "C" fn compute(drawing_buffer: *mut DAtom, buffer_size: u64) {
         logic_buffer[i as usize] = Atom { entity_tag: 42 };
         unsafe { *drawing_buffer.add(i as usize)  = DAtom { material: 42, obsolete: true }};
     }
+}
+
+/// Pre: Index refers to element in grid
+/// Post: Eight element array of the eight neighbours (toroidal geometry) that surrounds the pixel at the given index
+/// The indexes of the array look like:
+/// ```txt
+/// 012
+/// 3X4
+/// 567```
+fn find_neighbors(index: usize, buffer: *mut Atom, width: usize, height: usize) -> [EntityTag; 8] {
+    let x: isize = (index%width) as isize;
+    let y: isize = (index/width) as isize;
+    let mut counter = 0;
+    let mut result = [0 ;8];
+    for i in 0..3 {
+        for j in 0..3 {
+            let x1:isize = (x-1 + j)%(width as isize);
+            let y1:isize = (y-1 + i)%(height as isize);
+            if i != 1 || j != 1 {
+                unsafe {result[counter] = (*buffer.add((x1 + y1*width as isize) as usize)).entity_tag};
+                counter += 1;
+            }
+        }
+    }
+    result
+}
+
+#[test]
+fn test_neighbours() {
+    let mut array = [Atom{entity_tag: 0};12];
+    for i in 0..12 {
+        array[i] = Atom{entity_tag:i as u64};
+    }
+    let tags = find_neighbors(2,  array.as_mut_ptr(), 4, 3);
+
+    dbg!(tags);
 }
