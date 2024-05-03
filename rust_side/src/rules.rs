763b4977b01e3c70616fe1330c714b4bd607dfa5
casenc <amatgilvinyes@gmail.com>
2024-05-04 01:42:33 +0200

    Minor tweaks to neighbouring

--- rust_side/src/rules.rs
+++ rust_side/src/rules.rs
@@ -1,9 +1,5 @@
 use crate::*;
 
-fn positive_mod(i: isize, n: isize) -> usize {
-    ((i % n + n) % n) as usize
-}
-
 /// Pre: Index refers to element in grid
 /// Post: Eight element array of the eight neighbours (toroidal geometry) that surrounds the pixel at the given index
 /// The indexes of the array look like:
@@ -13,16 +9,15 @@ fn positive_mod(i: isize, n: isize) -> usize {
 /// 567
 /// ```
 pub fn find_neighbours(index: usize, buffer: *mut Atom, width: usize, height: usize) -> [EntityTag; 8] {
-    let x: isize = (index%width) as isize;
-    let y: isize = (index/width) as isize;
+    let (x, y) = i_to_xy(index, width);
     let mut counter = 0;
-    let mut result = [0 ;8];
+    let mut result: [EntityTag; 8]= [0; 8];
     for i in 0..3 {
         for j in 0..3 {
-            let x1:usize = positive_mod((x-1 + j) as isize, width as isize);
-            let y1:usize = positive_mod((y-1 + i) as isize, height as isize);
+            let x1: usize = (x + width  - 1 + j) % width;
+            let y1: usize = (y + height - 1 + i) % height;
             if i != 1 || j != 1 {
-                unsafe {result[counter] = (*buffer.add(x1 + y1*width)).entity_tag};
+                unsafe { result[counter] = (*buffer.add(xy_to_i(x1, y1, width))).entity_tag }
                 counter += 1;
             }
         }
@@ -63,13 +58,15 @@ pub fn move_up(i: usize, width: usize, height: usize) -> usize {
     x = (x + height - 1) % height;
     xy_to_i(x, y, width)
 }
+
 #[test]
 fn test_neighbours() {
-    let mut array = [Atom{entity_tag:0 as u64, priority: 1, material: 1, obsolete: false};12];
+    let mut array = [ Atom::NULL; 12 ];
     for i in 0..12 {
         array[i] = Atom{entity_tag:i as u64, priority: 1, material: 1, obsolete: false};
     }
     let tags = find_neighbours(2,  array.as_mut_ptr(), 4, 3);
 
     dbg!(tags);
+    assert_eq!(tags, [9, 10, 11, 1, 3, 5, 6, 7]);
 }
