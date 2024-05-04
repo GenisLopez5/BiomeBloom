f41a6ba11b081760d5584d172c9d92313b267529
Gerard <gerardfelez2@gmail.com>
2024-05-04 10:30:50 +0200

    implemented neighbours for position

--- rust_side/src/position.rs
+++ rust_side/src/position.rs
@@ -1,3 +1,5 @@
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Position {
     x: usize,
     y: usize,
@@ -30,6 +32,22 @@ impl Position {
     pub fn move_left(&self, width: usize) -> Self {
         let x = (self.x + width - 1) % width;
         Self { x, ..*self }
+    }
+    pub fn neighbours(&self, width: usize, height: usize) -> [Position; 8]{
+
+        let mut counter = 0;
+        let mut result = [Position::new(0,0);8];
+        for i in 0..3 {
+            for j in 0..3 {
+                if i != 1 || j != 1 {
+                    let x1:usize = (self.x + j + width - 1)%width;
+                    let y1:usize = (self.y + i + height - 1)%height;
+                    result[counter] = Position::new(x1,y1);
+                    counter += 1;
+                }
+            }
+        }
+        result
     }
 }
 
--- scripts/linux_deps.sh
+++ scripts/linux_deps.sh
@@ -1,2 +1,2 @@
 sudo apt-get install -y libpthread-stubs0-dev libgl1-mesa-dev libx11-dev libxrandr-dev libfreetype6-dev libglew-dev libjpeg8-dev libsndfile1-dev libopenal-dev libudev-dev libxcb-image0-dev libjpeg-dev libflac-dev
-sudo apt-get libsfml-dev
+sudo apt-get install libsfml-dev
