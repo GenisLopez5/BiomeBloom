a3ba0fdb46e8831b78710b5e6c6cb3004874c5b6
casenc <amatgilvinyes@gmail.com>
2024-05-04 16:46:39 +0200

    Updated headers, something seems wrong

--- C++/rust_functions.h
+++ C++/rust_functions.h
@@ -1,3 +1,4 @@
 #include "DAtom.hh"
 #include "MouseInfo.hh"
-extern "C" void compute(DAtom *ptr, int buffer_width, int buffer_height, MouseInfo mouse);
+extern "C" void compute(DAtom *ptr, int buffer_width, int buffer_height);
+extern "C" void update_mouse(DAtom *ptr, MouseInfo mouse);
--- rust_side/src/lib.rs
+++ rust_side/src/lib.rs
@@ -47,51 +47,22 @@ enum Entity {
     Fire
 }
 
-/// First time set up of logical buffer (initial state of simulation). All Atoms should be marked as obsolete, here
-fn init_logic_buffer(logic_buffer: &mut Vec<Atom>, buffer_width: usize, buffer_height: usize) {
-    println!("Initializing ");
-    for _ in 0..buffer_width*buffer_height {
-        logic_buffer.push(Atom::NULL)
-    }
-    let ant_pos = Position::new(buffer_width / 2, buffer_height / 2);
-    logic_buffer[ant_pos.as_idx(buffer_width, buffer_height)] = Atom {
-        entity_tag: Entity::Ant as i64,
-        priority: 1,
-        obsolete: true,
-    };
-}
-
 // Internal buffer, in case we need to add things like Lifetimes or whatever
 static LOGIC_BUFFER: Mutex<Vec<Atom>> = Mutex::new(Vec::new());
 
 #[no_mangle]
 pub extern "C" fn compute(
     drawing_buffer: *mut DAtom,
-    buffer_width: u64,
-    buffer_height: u64,
-    mouse: Mouse,
+    buffer_width: i64,
+    buffer_height: i64,
 ) {
-    let buffer_height = buffer_height as usize;
-    let buffer_width = buffer_width as usize;
-    let buffer_size = buffer_height * buffer_width;
     let mut logic_buffer = LOGIC_BUFFER.lock().unwrap();
-    if logic_buffer.is_empty() {
-        init_logic_buffer(&mut *logic_buffer, buffer_width, buffer_height);
-    }
-
-    println!("Click is: {}", mouse.clicked);
-    dbg!(mouse);
-    if mouse.clicked && mouse.posx >= 0_i64 && mouse.posy >= 0_i64 {
-        dbg!(mouse);
-        let pos = Position::new(mouse.posx as usize, mouse.posy as usize);
-        logic_buffer[pos.as_idx(buffer_width, buffer_height)] = Atom {
-                entity_tag: mouse.selected_tag,
-                priority: 2,
-                obsolete: true,
-        };
-    }
-
     let mut new_logic_buffer = logic_buffer.clone();
+
+    let (buffer_width, buffer_height): (usize, usize) = (buffer_width.try_into().unwrap(), buffer_height.try_into().unwrap());
+    let buffer_size = buffer_height * buffer_width;
+
+    init_logic_buffer_if_needed(&mut *logic_buffer, buffer_width, buffer_height);
 
     for i in 0..buffer_size {
         for p in 0..=u8::MAX {
@@ -104,10 +75,44 @@ pub extern "C" fn compute(
     println!("Finished calculating frame");
 
     *logic_buffer = new_logic_buffer;
-    // Update drawing buffer with the logic one
-    for i in 0..buffer_size {
+    get_buffer_parity(drawing_buffer, &mut logic_buffer, buffer_width, buffer_height)
+}
+
+#[no_mangle]
+pub extern "C" fn update_mouse(mouse: Mouse, drawing_buffer: *mut DAtom, logic_buffer: &mut Vec<Atom>, buffer_width: usize, buffer_height: usize) {
+    println!("Click is: {}", mouse.clicked);
+    if mouse.clicked && mouse.posx >= 0_i64 && mouse.posy >= 0_i64 {
+        dbg!(mouse);
+        let pos = Position::new(mouse.posx as usize, mouse.posy as usize);
+        logic_buffer[pos.as_idx(buffer_width, buffer_height)] = Atom {
+                entity_tag: mouse.selected_tag,
+                priority: 2,
+                obsolete: true,
+        };
+    }
+
+    get_buffer_parity(drawing_buffer, logic_buffer, buffer_width, buffer_height)
+}
+
+fn get_buffer_parity(drawing_buffer: *mut DAtom, logic_buffer: &mut Vec<Atom>, buffer_width: usize, buffer_height: usize) {
+    for i in 0..buffer_height * buffer_width {
         unsafe {
             *drawing_buffer.add(i) = logic_buffer[i].into();
         }
     }
+}
+
+/// First time set up of logical buffer (initial state of simulation). All Atoms should be marked as obsolete, here
+fn init_logic_buffer_if_needed(logic_buffer: &mut Vec<Atom>, buffer_width: usize, buffer_height: usize) {
+    if !logic_buffer.is_empty() { return; }
+    println!("Initializing ");
+    for _ in 0..buffer_width*buffer_height {
+        logic_buffer.push(Atom::NULL)
+    }
+    let ant_pos = Position::new(buffer_width / 2, buffer_height / 2);
+    logic_buffer[ant_pos.as_idx(buffer_width, buffer_height)] = Atom {
+        entity_tag: Entity::Ant as i64,
+        priority: 1,
+        obsolete: true,
+    };
 }
