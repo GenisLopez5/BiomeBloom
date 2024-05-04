7e61f6f9e8ac9f6e669852b62b704d6f0dce4b49
GenisLopez5 <misterproxx@gmail.com>
2024-05-04 02:03:36 +0200

    Changed Renderer

--- C++/Renderer.hh
+++ C++/Renderer.hh
@@ -6,10 +6,16 @@
 using namespace std;
 using namespace sf;
 
-class Renderer {
-private:
-  int render_buffer_size;
-  vector<Sprite> sprites;
+class Renderer{
+    private:
+        int render_buffer_size;
+        vector<Sprite> sprites;
+        vector<Texture> textures;
+
+        void set_new_texture(const DAtom& d_atom, Sprite& sprite);
+    public:
+        RenderWindow window;
+        DAtom* render_buffer;
 
 public:
   RenderWindow window;
