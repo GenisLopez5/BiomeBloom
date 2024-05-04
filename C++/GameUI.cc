19521e7780054d1de0cafc4533d57a9b181bee3c
GenisLopez5 <misterproxx@gmail.com>
2024-05-04 22:30:07 +0200

    Fixed Canvas (i guess)

--- C++/GameUI.cc
+++ C++/GameUI.cc
@@ -1,12 +1,13 @@
 #include "GameUI.hh"
 
-void GameUI::Setup() {
+GameUI::GameUI(int size_x, int size_y) : Canvas(size_x, size_y) {
+    
     _PaletteText =
         addText("_PaletteImg", "Selected material: ", "../data/defaultFont.otf",
                 30, BottomMiddle + sf::Vector2f(50, 50), sf::Vector2f(0.5, 0));
     _PaletteImg = addSprite("_PaletteImg", "../data/palette.png", BottomMiddle,
                             sf::Vector2f(0.5, 0));
-    _PaletteSelectedImg =
+    _PaletteSelectedImg = 
         addSprite("_PaletteSelectedImg", "../data/PaletteSelected.png",
                   sf::Vector2f(BottomMiddle.x - 75 * 4, BottomMiddle.y),
                   sf::Vector2f(0.5, 0));
